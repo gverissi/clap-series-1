88cd5e7fe3307807afa36efc4a9625e5ecfe5eba
greg <greg.verissimo@gmail.com>
2020-06-08 22:28:35 +0200

    Implement bestCombo.js

--- src/bestCombo.js
+++ src/bestCombo.js
@@ -18,8 +18,155 @@
  * 
  */
 
-function bestCombo(cards) {
-  // CODE HERE
+import { occurences } from "../src/occurences";
+import { orderCards } from "../src/orderCards";
+import { isAFlush } from "../src/isAFlush";
+import { isAPair } from "../src/isAPair";
+
+const CARDS = {
+	as: "A",
+	king: "K",
+	queen: "Q",
+	jack: "J",
+	ten: "10",
+	nine: "9",
+	height: "8",
+	seven: "7",
+	six: "6",
+	five: "5",
+	four: "4",
+	three: "3",
+	two: "2"
 }
 
-export { bestCombo };
+function bestCombo(cards) {
+
+	cards = orderCards(cards)
+	let cardsValue = getCardsValue(cards)
+	let cardsType = getCardsType(cards)
+	
+	// Check for a full
+	let hand = []
+	let occur = occurences(cards)
+	let isAThree = false
+	let isATwo = false
+	for (let [key, value] of Object.entries(occur)) {
+		if (value == 3 && isAThree == false) {
+			isAThree = true
+			for (var i = 0; i < cards.length; i++) {
+				if (cardsValue[i] == CARDS[key]) hand.push(cards[i])
+			}
+		}
+	}
+	for (let [key, value] of Object.entries(occur)) {
+		if (value == 2 && isATwo == false) {
+			isATwo = true
+			for (var i = 0; i < cards.length; i++) {
+				if (cardsValue[i] == CARDS[key]) hand.push(cards[i])
+			}
+		}
+	}
+	if (isAThree && isATwo) {
+		return hand
+	}
+
+	// Check for a flush
+	if (isAFlush(cards)) {
+		let hand = []
+		let types = ["d", "c", "h", "s"]
+		types.forEach(
+			type => {
+				let nbOcc = nbOccurences(cardsType, type)
+				if (nbOcc > 4) {
+					let indexes = allIndexOf(cardsType, type)
+					for (let i = 0; i < 5; i++) {
+						hand.push(cards[indexes[i]])
+					}
+					return
+				}
+			}
+		)
+		return hand
+	}
+
+	// Check for a pair
+	if (isAPair(cards)) {
+		let hand = []
+		let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+		values.forEach(
+			value => {
+				let nbOcc = nbOccurences(cardsValue, value)
+				if (nbOcc == 2) {
+					let indexes = allIndexOf(cardsValue, value)
+					for (let i = 0; i < 2; i++) {
+						hand.push(cards[indexes[i]])
+					}
+					let i = 0
+					let j = 0
+					while (j < 3) {
+						if (!indexes.includes(i)) {
+							hand.push(cards[i])
+							j++
+						}
+						i++
+					}
+					return
+				}
+			}
+		)
+		return hand
+	}
+
+	// Check for a hauteur
+	hand = []
+	for (let i = 0; i < 5; i++) {
+		hand.push(cards[i])
+	}
+	return hand
+
+}
+
+
+function getCardsValue(cards) {
+	let cardsValue = []
+	let value = ""
+	cards.forEach(
+		card => {
+			value = card.charAt(0)
+			if (value == "1") value = value + "0"
+			cardsValue.push(value)
+		}
+	)
+	return cardsValue
+}
+
+function getCardsType(cards) {
+	let cardsType = []
+	let type = ""
+	cards.forEach(
+		card => {
+			type = card.charAt(card.length-1)
+			cardsType.push(type);
+		}
+	)
+	return cardsType
+}
+
+function allIndexOf(arr, value) {
+	if (arr.indexOf(value) >= 0) {
+		let res = []
+		for( let i = arr.indexOf(value); i >= 0; i = arr.indexOf(value,i+1) ) {
+			res.push(i);
+		}
+		return res
+	}
+	else return false
+}
+
+function nbOccurences(arr, value) {
+	let indexes = allIndexOf(arr, value)
+	if (indexes != false) return indexes.length
+	else return false
+}
+
+export { bestCombo }
